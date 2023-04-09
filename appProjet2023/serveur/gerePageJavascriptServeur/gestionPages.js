8ec7a7dbb7289a78e98f7d22ff7117b5acff87d1
hiro-ronui <hirohunter@live.Fr>
2023-04-10 01:26:19 +0200

    tes

--- appProjet2023/serveur/gerePageJavascriptServeur/gestionPages.js
+++ appProjet2023/serveur/gerePageJavascriptServeur/gestionPages.js
@@ -22,27 +22,28 @@ var gestionPages = {
     this.reponse.end();
   },
 
-  genereDonneeAEnvoyer: function () {
-    var donnee = {};
-    var dossier = '';
-
-    if (this.extension === '.html' || this.url.pathname === '/') {
-      if (this.url.pathname === '/') {
-        this.url.pathname = '/index.html';
-      }
-      dossier = 'html';
-      donnee.contentType = 'text/html';
-      donnee.content = this.generePageHtml(dossier);
+    genereDonneeAEnvoyer : function(){
+        var donnee = {};
+        var dossier = "";
+        
+        if(this.extension === ".html" || this.url.pathname==="/"){
+           if(this.url.pathname=== "/"){
+              this.url.pathname = "/index.html";
+            }
+            dossier = "html";
+            donnee.contentType = "text/html";
+            donnee.content = this.generePageHtml(dossier);
+        }
+        return donnee;
+    },
+    generePageHtml : function(dossier){
+        var pageHTML = "";
+        var headerHTML = fs.readFileSync(dossier+"/header.html", "UTF-8");
+        var footerHTML = fs.readFileSync(dossier+"/footer.html", "UTF-8");
+        var page = ffs.readFileSync(dossier+this.url.pathname, "UTF-8");
+        pageHTML = headerHTML + page + footerHTML;
+        return pageHTML;
     }
-    return donnee;
-  },
-  generePageHtml: function (dossier) {
-    var pageHTML = '';
-    var headerHTML = fs.readFileSync('../' + dossier + '/header.html', 'UTF-8');
-    var footerHTML = fs.readFileSync('../' + dossier + '/footer.html', 'UTF-8');
-    var page = fs.readFileSync('../' + dossier + this.url.pathname, 'UTF-8');
-    pageHTML = headerHTML + page + footerHTML;
-    return pageHTML;
-  },
-};
+
+    }
 module.exports = gestionPages;
